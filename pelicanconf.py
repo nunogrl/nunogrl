6ed54bceb00ab5adcc9e26b923e0ad1faba186fc
Nuno Leitao <nunogrl@gmail.com>
2020-10-05 15:37:02 +0100

    changes to adjust to new template

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,11 +6,12 @@ AUTHOR = u'Nuno Leitao'
 SITENAME = u'nunogrl.com'
 SITEURL = ''
 PATH = 'content'
+DEBUG = True
+
 # DEFAULT_CATEGORY = 'personal'
 
-ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
-ARTICLE_URL = '{date:%Y}/{slug}.html'
-
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
 
 TIMEZONE = 'Europe/London'
 
@@ -57,23 +58,103 @@ MENUITEMS = (
 )
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'atom.xml'
+TAG_FEED_ATOM = 'categories/{slug}/atom.xml'
+CATEGORY_FEED_ATOM = "category/{slug}/atom.xml"
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+DISPLAY_FEEDS_ON_SIDEBAR = True
+DISPLAY_TAGS_ON_SIDEBAR = False
 
-# Blogroll
-LINKS = (('My Shaarli', 'http://bookmarks.barbearclassico/'),
-          ('Another social link', '#'),)
+RELATED_POSTS_MAX = 10
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.with_', 'jinja2.ext.do']}
 
-DEFAULT_PAGINATION = 10
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
 
-THEME = "themes/pelican-clean-blog"
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
+
+TAG_URL = 'categories/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+
+ARCHIVES_URL = 'archives/'
+ARCHIVES_SAVE_AS = ARCHIVES_URL + 'index.html'
+
+AUTHOR_URL = 'authors/{slug}/'
+AUTHOR_SAVE_AS = AUTHOR_URL + 'index.html'
+
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ['related_posts', 'series', 'sitemap', 'tag_cloud', 'assets', 'share_post']
+
+SITEMAP = {
+    'format': 'xml',
+}
+
+DEFAULT_DATE_FORMAT = '%d/%m/%Y'
+
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/nunogrl'),
+    ('GitHub', 'https://github.com/nunogrl'),
+)
+
+DEFAULT_PAGINATION = 9
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+STATIC_PATHS = [
+    'images', 'code', 'notebooks', 'files', 'extra/CNAME', 'extra/robots.txt'
+]
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+}
+
+TWITTER_USERNAME = 'thedigicat'
+
+THEME = "themes/editorial"
+
+FAVICON = 'images/global/favicon.jpg'
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+        'markdown.extensions.toc': {
+            'permalink': True
+        },
+        'mdx_video': {},
+    },
+    'output_format': 'html5',
+}
+
+QUOTES = [
+    {
+        'quote': "I've seen things you people wouldn't believe. Attack ships"
+        " on fire off the shoulder of Orion. I watched C-beams glitter in the"
+        " dark near the Tannhäuser Gate. All those moments will be lost in"
+        " time, like tears in rain. Time to die.",
+        'source': 'Blade Runner, 1982'
+    },
+    {
+        'quote': "Look at this. It’s worthless — ten dollars from a vendor in"
+        " the street. But I take it, I bury it in the sand for a thousand"
+        " years, it becomes priceless.",
+        'source': 'Raiders of the Lost Ark, 1981'
+    },
+    {
+        'quote': "Roads? Where we're going, we don't need... roads.",
+        'source': 'Back to the Future, 1985'
+    },
+]
+
+
 # THEME = "themes/bricks"
 # THEME = "themes/blue-penguin"
 
